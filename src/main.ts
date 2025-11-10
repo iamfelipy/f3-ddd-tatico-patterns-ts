adda989997f8aa574e325ab3e9a76a899c2e7398
felipy <felipy81br@gmail.com>
2025-11-10 19:51:08 -0300

    feat: update OrderItem to include product ID and quantity

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,6 @@ customer.Address = address
 customer.activate()
 
 // Agregado com relação de Objeto
-const item1  = new OrderItem("1", "item 1", 10);
-const item2  = new OrderItem("2", "item 2", 15);
+const item1  = new OrderItem("1", "item 1", 10, "prod1", 1);
+const item2  = new OrderItem("2", "item 2", 15, "prod2", 1);
 const order = new Order("1", "123", [item1, item2])
