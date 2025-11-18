6fa12f94f8015af961311337667ecbd6bfd1aa2c
felipy <felipy81br@gmail.com>
2025-11-18 19:58:48 -0300

    test: event-dispatcher: it should register an event handler

---
+++ src/domain/event/@shared/event/event-dispatcher.ts
@@ -0,0 +1,28 @@
+import EventDispatcherInterface from "./event-dispatcher.interface";
+import EventHandlerInterface from "./event-handler.interface";
+import eventInterface from "./event.interface";
+
+export default class EventDispatcher implements EventDispatcherInterface {
+  private eventHandlers: { [eventName: string]: EventHandlerInterface[] } = {};
+
+  get getEventHandlers(): { [eventName: string]: EventHandlerInterface[] } {
+    return this.eventHandlers;
+  }
+
+  register(eventName: string, eventHandler: EventHandlerInterface): void {
+    if (!this.eventHandlers[eventName]) {
+      this.eventHandlers[eventName] = [];
+    }
+    this.eventHandlers[eventName].push(eventHandler);
+  }
+
+  notify(event: eventInterface): void {
+    throw new Error("Method not implemented.");
+  }
+  unregister(eventName: string, eventHandler: EventHandlerInterface): void {
+    throw new Error("Method not implemented.");
+  }
+  unregisterAll(): void {
+    throw new Error("Method not implemented.");
+  }
+}
